c6b7140022a85f0565266825cde61bc557a50469
SaffronWilliamson <saffron.havana99@gmail.com>
2025-03-06 12:35:23 +0000

    Added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './Dictionary.css';
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState('');
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for the definition of ${keyword}..`);
+
+    let apiKey = 'ce7c3ee7eo0a7079t0e5079cd0efcb43';
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
@@ -18,3 +30,5 @@ export default function Dictionary() {
     </div>
   );
 }
+// let apiKey = "ce7c3ee7eo0a7079t0e5079cd0efcb43"
+// let apiUrl = https://api.shecodes.io/dictionary/v1/define?word=book&key=${apiKey}
